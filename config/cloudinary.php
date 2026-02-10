b480cab0b2a7c1734a4e085ecc6654727338490d
LOCADOC <administrateur@DOMetVIE-IT.local>
2026-02-10 14:47:29 +0100

    Fix Cloudinary config structure

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -3,7 +3,5 @@
 return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key' => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'secure' => env('CLOUDINARY_SECURE_URL', true),
-    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+    'api_secret' => env('CLOUDINARY_API_SECRET')
 ];
